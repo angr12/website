cb593ebfe3c83f04041797e7d41ddd0a983473bf
angr12 <rla120@ic.ac.uk>
2022-11-08 19:12:32 +0000

    feat: implemented initial portfolio page

--- components/intro.tsx
+++ components/intro.tsx
@@ -25,7 +25,7 @@ export default function Intro() {
           letterSpacing={{ sm: '-1.2px', md: '-1.8px' }}
           position="relative"
         >
-          <Box color="displayColor" as="span">Your Name</Box>
+          <Box color="displayColor" as="span">Ryan Ang</Box>
           .
         </Heading>
       </SlideFade>
@@ -36,7 +36,7 @@ export default function Intro() {
       >
         <Box w={['85vw', '40vw']}>
         <Text fontSize="display3" color="textSecondary">
-          Some description about yourself. Lorem ipsum and all that good stuff. Some more waffle to fill up the space.
+          3rd Year Molecular Bioengineering Student at Imperial College London
         </Text>
         </Box>
       </SlideFade>
@@ -46,7 +46,7 @@ export default function Intro() {
         transition={{ enter: { duration: 0.4, delay: 1.0 } }}
       >
         <Stack isInline spacing={4}>
-          <Link href="https://github.com/" isExternal>
+          <Link href="https://github.com/angr12" isExternal>
             <Button
               leftIcon={<FaGithub color="#5F99FF" />}
               position="static"
@@ -57,7 +57,7 @@ export default function Intro() {
               Github
             </Button>
           </Link>
-          <Link href="https://linkedin.com/" isExternal>
+          <Link href="https://www.linkedin.com/in/rla120/" isExternal>
             <Button
               leftIcon={<FaLinkedin color="#5F99FF" />}
               position="static"
@@ -68,7 +68,7 @@ export default function Intro() {
               LinkedIn
             </Button>
           </Link>
-          <Link href="mailto:gdsc@imperial.ac.uk" isExternal>
+          <Link href="mailto:rla120@ic.ac.uk" isExternal>
             <Button
               leftIcon={<FaEnvelope fill="#5F99FF" />}
               transition="0.3s"
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,28 @@
 import { NextPage } from "next"
 import Container from "../components/container"
+import Intro from "../components/intro"
+import {Stack, Wrap, WrapItem} from "@chakra-ui/react"
+import GdscCanvas from "../components/gdsc-canvas"
 
 const Home: NextPage = () => {
   return (
     <>
-      <Container>        
+      <Container>  
+        <Stack
+        spacing = {'144px'}
+        justifyContent = 'center'
+        alignItems = "flex-start"
+        px = {{base: '5vw', md: "7.5vw"}}
+        mt = {{base: "12.5vh", md: "22.5vh"}}
+        >
 
+        <Wrap>
+          <WrapItem> <Intro/> </WrapItem>
+          <WrapItem> <GdscCanvas/> </WrapItem>
+
+
+          </Wrap>
+          </Stack>      
       </Container>
     </>
   )
